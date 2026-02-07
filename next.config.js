8927a71f8368a26c97637f02a9ea41298974a4b5
kunxl <kunalx9966@gmail.com>
2026-02-07 19:09:37 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [],
-  },
-}
+  output: 'export',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
